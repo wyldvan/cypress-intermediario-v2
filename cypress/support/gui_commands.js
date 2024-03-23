167baeeff64ed710b733c7e9d3a96cc73f1a91f0
wyldvan <wyldvan08@gmail.com>
2024-03-23 18:29:41 -0400

    feat: todays studies

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -12,3 +12,17 @@ Cypress.Commands.add('login', (
 
     login()
 })
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+})
